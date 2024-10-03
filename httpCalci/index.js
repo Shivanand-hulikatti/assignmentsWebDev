8f872e04ad7ef539bdf07c8acb0f85083c55d747
Shivanand-hulikatti <01fe23bcs268@kletech.ac.in>
2024-10-04 00:41:51 +0530

    http calci

---
+++ httpCalci/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+app.get('/sum',(req,res)=>{
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+    res.json({
+        ans:a+b
+    })
+});
+
+app.get('/substract',(req,res)=>{
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+    res.json({
+        ans:a-b
+    })
+});
+
+app.get('multiply',(req,res)=>{
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+    res.json({
+        ans:a*b
+    })
+});
+
+app.get('divide',(req,res)=>{
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+    res.json({
+        ans:a/b
+    })
+});
+
+app.listen(3000);
